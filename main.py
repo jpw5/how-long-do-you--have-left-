2e5d9d33473d07ecc45490492d13a8f9eabdcf0c
JarrettWilliam1 <>
2021-07-12 13:23:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+age = input("What is your current age?")
+
+day = (90 - int(age)) * 365
+month = (90 - int(age)) * 12
+week = (90 - int(age)) * 52
+
+print(f"You have {day} days, {week} weeks, and {month} months left.")
+
+
+
+
+
+
+
+
